130d8d2c5a8d3f6bbdd9542fe6df5dfe19312b0d
josemaradiaga <josemarad25@gmail.com>
2021-02-07 16:50:08 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown   (secondsTomidnoght)
+    while secondsTomidnoght > 0 
+        puts "#{secondsTomidnoght} SECOND(S)!"
+        secondsTomidnoght -= 1
+    end 
+    prints "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep  (secondsTomidnoght)
+    while secondsTomidnoght > 0
+        puts "#{secondsTomidnoght} SECOND(S)!"
+        secondsTomidnoght -= 1
+        #sleep (1) 
+    end
+    prints "HAPPY NEW YEAR!"
+end
+
+
